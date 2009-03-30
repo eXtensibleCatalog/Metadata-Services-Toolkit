10af86ad6fac35b285304732b78565390f622e03
tharamurali@library.rochester.edu <tharamurali@library.rochester.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2009-03-30 21:08:18 +0000

    Updating Log files
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/trunk@67 72d1a826-0e72-11de-b5bf-434079a3519f

---
+++ src/webapps/MetadataServicesToolkit/page-resources/js/all_services.js
@@ -0,0 +1,18 @@
+ /*
+  * Copyright (c) 2009 University of Rochester
+  *
+  * This program is free software; you can redistribute it and/or modify it under the terms of the MIT/X11 license. The text of the
+  * license can be found at http://www.opensource.org/licenses/mit-license.php and copy of the license can be found on the project
+  * website http://www.extensiblecatalog.org/.
+  *
+  */
+
+YAHOO.namespace("xc.mst.services");
+
+YAHOO.xc.mst.services = {
+
+ downloadFile: function(type,id)
+            {
+                window.location = "pages/logs/downloadLogFile.jsp?logType="+type+"&id="+id;
+            }
+}
