dec618d6a0901798874e6b5bf117d2fbb8208375
tharamurali@library.rochester.edu <tharamurali@library.rochester.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2009-04-15 17:06:25 +0000

    PD files deleted
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/trunk@168 72d1a826-0e72-11de-b5bf-434079a3519f

--- src/java/xc/mst/tags/CurrentProcessStatus.java
+++ src/java/xc/mst/tags/CurrentProcessStatus.java
@@ -35,7 +35,7 @@ public class CurrentProcessStatus extends SimpleTagSupport
             {
 
                     
-                    out.println(currentProcess);
+                    out.print("<xxx id='currentProcess'>"+currentProcess+"</xxx>");
                     Object tempObject =  getJspContext().findAttribute("serviceBarDisplay");
                     if(tempObject!=null)
                     {
@@ -43,25 +43,29 @@ public class CurrentProcessStatus extends SimpleTagSupport
                         System.out.println("ServiceDisplay is "+serviceBarDisplay);
                         if(serviceBarDisplay.equalsIgnoreCase("pause"))
                         {
-                            out.println("<button style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.services.alterStatus(\"pause\");' name='Pause'>Pause</button> &nbsp;&nbsp;&nbsp;");
-                            out.println("<button class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.services.alterStatus(\"abort\");' name='Abort'>Abort</button>");
+                            out.print("<button style=\"display:none;\" id='resumeButton' style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"resume\");' name='Resume'>Resume</button> &nbsp;&nbsp;&nbsp;");
+                            out.print("<button id='pauseButton' style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"pause\");' name='Pause'>Pause</button> &nbsp;&nbsp;&nbsp;");
+                            out.print("<button id='abortButton' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"abort\");' name='Abort'>Abort</button>");
                         }
                         else
                         {
-                            out.println("<button style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.services.alterStatus(\"resume\");' name='Resume'>Resume</button> &nbsp;&nbsp;&nbsp;");
-                            out.println("<button class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.services.alterStatus(\"abort\");' name='Abort'>Abort</button>");
+                            out.print("<button id='resumeButton' style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"resume\");' name='Resume'>Resume</button> &nbsp;&nbsp;&nbsp;");
+                            out.print("<button style=\"display:none;\" id='pauseButton' style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"pause\");' name='Pause'>Pause</button> &nbsp;&nbsp;&nbsp;");
+                            out.print("<button id='abortButton' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"abort\");' name='Abort'>Abort</button>");
                         }
                     }
                     else
                     {
-                         out.println("<button style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.services.alterStatus(\"pause\");' name='Pause'>Pause</button> &nbsp;&nbsp;&nbsp;");
-                         out.println("<button class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.services.alterStatus(\"abort\");' name='Abort'>Abort</button>");
+                         out.print("<button style=\"display:none;\" id='resumeButton' style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"resume\");' name='Resume'>Resume</button> &nbsp;&nbsp;&nbsp;");
+                         out.print("<button id='pauseButton' style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"pause\");' name='Pause'>Pause</button> &nbsp;&nbsp;&nbsp;");
+                         out.print("<button id='abortButton' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"abort\");' name='Abort'>Abort</button>");
                     }
             }
             else //there is no current process being run by the MST
             {
-                out.println("<button disabled style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.services.alterStatus(\"pause\");' name='Pause'>Pause</button> &nbsp;&nbsp;&nbsp;");
-                out.println("<button disabled class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.services.alterStatus(\"abort\");' name='Abort'>Abort</button>");
+                out.print("<button style=\"display:none;\" id='resumeButton' style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"resume\");' name='Resume'>Resume</button> &nbsp;&nbsp;&nbsp;");
+                out.print("<button id='pauseButton' disabled style='vertical-align:bottom;' class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"pause\");' name='Pause'>Pause</button> &nbsp;&nbsp;&nbsp;");
+                out.print("<button id='abortButton' disabled class='xc_button' type='button' onclick='javascript:YAHOO.xc.mst.serviceStatusBar.alterStatus(\"abort\");' name='Abort'>Abort</button>");
             }
         }
         catch(Exception e)
