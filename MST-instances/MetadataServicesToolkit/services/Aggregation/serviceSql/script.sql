6ce15abf5525ffd94087da8950f81c56bffd5234
sranganathan@library.rochester.edu <sranganathan@library.rochester.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2010-03-04 20:03:35 +0000

    added index
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/trunk@734 72d1a826-0e72-11de-b5bf-434079a3519f

--- MST-instances/MetadataServicesToolkit/services/Aggregation/serviceSql/script.sql
+++ MST-instances/MetadataServicesToolkit/services/Aggregation/serviceSql/script.sql
@@ -10,14 +10,18 @@ DROP TABLE IF EXISTS in_processed_identifiers;
 CREATE TABLE in_processed_identifiers
 (
   in_processed_identifiers_id INT(11) NOT NULL AUTO_INCREMENT,
-  oai_id TEXT  NOT NULL,
-  oclc_value TEXT  NULL,
-  lccn_value TEXT  NULL,
-  isbn_value TEXT  NULL,
-  issn_value TEXT  NULL,
-  PRIMARY KEY(in_processed_identifiers_id)
+  oai_id VARCHAR(255)  NOT NULL,
+  oclc_value VARCHAR(255)  NULL,
+  lccn_value VARCHAR(255)  NULL,
+  isbn_value VARCHAR(255)  NULL,
+  issn_value VARCHAR(255)  NULL,
+  PRIMARY KEY(in_processed_identifiers_id),
+  INDEX idx_oclc_value (oclc_value),
+  INDEX idx_lccn_value (lccn_value),
+  INDEX idx_isbn_value (isbn_value),
+  INDEX idx_issn_value (issn_value),
+  CONSTRAINT UNIQUE INDEX idx_oai_id (oai_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 -- -------------------------------------------------------------
 -- Table structure for output_record
@@ -27,10 +31,11 @@ DROP TABLE IF EXISTS output_record;
 CREATE TABLE output_record
 (
   output_record_id  INT(11) NOT NULL AUTO_INCREMENT,
-  oai_id TEXT NOT NULL,
+  oai_id VARCHAR(255) NOT NULL,
   XML TEXT NOT NULL,
   updated boolean NOT NULL,
-  PRIMARY KEY(output_record_id)
+  PRIMARY KEY(output_record_id),
+  CONSTRAINT UNIQUE INDEX idx_oai_id (oai_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- -------------------------------------------------------------
@@ -41,10 +46,14 @@ DROP TABLE IF EXISTS predecessor_record;
 CREATE TABLE predecessor_record
 (
   predecessor_record_id INT(11) NOT NULL AUTO_INCREMENT,
-  predecessor_oai_id TEXT NOT NULL,
+  predecessor_oai_id VARCHAR(255) NOT NULL,
   output_record_id INT(11) NOT NULL,
   PRIMARY KEY(predecessor_record_id),
-  FOREIGN KEY(output_record_id) REFERENCES output_record(output_record_id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY(output_record_id) REFERENCES output_record(output_record_id) ON DELETE CASCADE ON 
+
+UPDATE CASCADE,
+  INDEX idx_predecessor_oai_id (predecessor_oai_id),
+  INDEX idx_output_record_id (output_record_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -57,7 +66,8 @@ DROP TABLE IF EXISTS held_records;
 CREATE TABLE held_records
 (
   held_records_id  INT(11) NOT NULL AUTO_INCREMENT,
-  held_oai_id TEXT NOT NULL,
-  parent_oai_id TEXT NOT NULL,
-  PRIMARY KEY(held_records_id)
+  held_oai_id VARCHAR(255) NOT NULL,
+  parent_oai_id VARCHAR(255) NOT NULL,
+  PRIMARY KEY(held_records_id),
+  INDEX idx_parent_oai_id (parent_oai_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
