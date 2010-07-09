661a5e71b3ed78004447afb56ca5e28c1b128e2d
banderson@library.rochester.edu <banderson@library.rochester.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2010-07-09 18:46:14 +0000

    done verifying that the new system produces same MARCNormalization output as old system.  yay!
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/branches/bens_perma_branch@964 72d1a826-0e72-11de-b5bf-434079a3519f

--- scripts/conv/norm_output_1.sh
+++ scripts/conv/norm_output_1.sh
@@ -1,10 +1,10 @@
-RECORD_PREFIX='<records xmlns="http://www.openarchives.org/OAI/2.0/"><record><header><identifier>oai:mst.rochester.edu:OAI_ID</identifier></header><metadata>'
+RECORD_PREFIX='<records xmlns="http://www.openarchives.org/OAI/2.0/"><record><header><identifier>oai:mst.rochester.edu:marcnormalization/@OAI_ID@</identifier><oldFileName>@OLD_FILENAME@</oldFileName></header><metadata>'
 RECORD_SUFFIX='</metadata></record></records>'
 OAI_ID=0
-#for fn in $(ls -1 ./mst-service/custom/MARCNormalization/test/input_records/orig-184/499*.xml | sed 's/.*\///g')
-for fn in $(ls -1 ./mst-service/custom/MARCNormalization/test/input_records/orig-184/*.xml | sed 's/.*\///g')
+#for fn in $(ls -1 ./mst-service/custom/MARCNormalization/test/input_records/orig-186/499*.xml | sed 's/.*\///g')
+for fn in $(ls -1 ./mst-service/custom/MARCNormalization/test/input_records/orig-186/*.xml | sed 's/.*\///g')
 do
-	f=./mst-service/custom/MARCNormalization/test/input_records/orig-184/$fn
+	f=./mst-service/custom/MARCNormalization/test/input_records/orig-186/$fn
 	file_part=$OAI_ID
 	if [ $OAI_ID -lt 10 ]
 	then
@@ -16,17 +16,20 @@ do
 	fi
 	echo 
 	echo 
-	ls -1 "./mst-service/custom/MARCNormalization/test/expected_output_records/orig-184-0.2.9/oai-mst.rochester.edu-MetadataServicesToolkit MARCNormalization 10${file_part}.xml"
-	of="./mst-service/custom/MARCNormalization/test/expected_output_records/orig-184-0.2.9/oai-mst.rochester.edu-MetadataServicesToolkit MARCNormalization 10${file_part}.xml"
-	ef=./mst-service/custom/MARCNormalization/test/expected_output_records/orig-184/$fn
+	OLD_FILENAME="oai-mst.rochester.edu-MetadataServicesToolkit MARCNormalization 10${file_part}.xml"
+	ls -1 "./mst-service/custom/MARCNormalization/test/expected_output_records/orig-186-0.2.9/${OLD_FILENAME}"
+	of="./mst-service/custom/MARCNormalization/test/expected_output_records/orig-186-0.2.9/${OLD_FILENAME}"
+	ef=./mst-service/custom/MARCNormalization/test/expected_output_records/orig-186/$fn
 	echo file_part: $file_part
+	echo OLD_FILENAME: $OLD_FILENAME
 	echo fn: $fn
 	echo f: $f
 	echo of: $of
 	echo ef: $ef
 	echo OAI_ID: $OAI_ID
 
-	RECORD_P=$(echo $RECORD_PREFIX | sed "s/OAI_ID/${OAI_ID}/g")
+	let OAI_ID=OAI_ID+1
+	RECORD_P=$(echo $RECORD_PREFIX | sed "s/@OAI_ID@/${OAI_ID}/g" | sed "s/@OLD_FILENAME@/${OLD_FILENAME}/g")
 	echo ${RECORD_P} > ${ef}.new
 	cat "${of}" | grep -v '<?xml' >> ${ef}.new
 	#cat "${of}"
@@ -36,5 +39,4 @@ do
 	#echo '<?xml version="1.0" encoding="UTF-8"?>' > ${ef}
 	cat ${ef}.new.2 > ${ef}
 	rm ${ef}.new*
-	let OAI_ID=OAI_ID+1
 done
