5848e59f6cabaacef492a9e5ca156d9fe7e8e21e
banderson@library.rochester.edu <banderson@library.rochester.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2010-09-01 15:17:41 +0000

    initial DCToXCTransformation
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/branches/bens_perma_branch@1102 72d1a826-0e72-11de-b5bf-434079a3519f

---
+++ mst-service/custom/DCToXCTransformation/src/xc/mst/services/dc2xc/dao/FooDAO.java
@@ -0,0 +1,11 @@
+package xc.mst.services.dc2xc.dao;
+
+import xc.mst.dao.BaseDAO;
+
+public class FooDAO extends BaseDAO {
+	
+	public void insertFoo(String name) {
+		this.jdbcTemplate.update("insert into foos (foo) values (?)", name);
+	}
+
+}
