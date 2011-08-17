122e9f84e6a241c58209596ee690f8ddf2cb8876
banderson@library.rochester.edu <banderson@library.rochester.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2011-08-17 16:47:12 +0000

    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/branches/bens_perma_branch@2435 72d1a826-0e72-11de-b5bf-434079a3519f

--- MST-instances/MST-instances/MetadataServicesToolkit/sql/service_repo.sql
+++ MST-instances/MST-instances/MetadataServicesToolkit/sql/service_repo.sql
@@ -1,15 +1,32 @@
-create table repo_name.record_links (
-	from_record_id   bigint       not null,
-	to_record_id     bigint       not null
-	/*,
-	KEY idx_links_from_record_id(from_record_id),
-	KEY idx_links_to_record_id(to_record_id)
-	*/
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-create table repo_name.prev_incoming_record_statuses (
-	record_id        bigint       not null,
-	status           char(1)      not null,
+CREATE SCHEMA IF NOT EXISTS `repo_name` ;
+USE `repo_name`;
 
-	primary key (record_id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-- -----------------------------------------------------
+-- Table `repo_name`.`record_links`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `repo_name`.`record_links` (
+  `from_record_id` BIGINT NOT NULL ,
+  `to_record_id` BIGINT NOT NULL )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `repo_name`.`prev_incoming_record_statuses`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `repo_name`.`prev_incoming_record_statuses` (
+  `record_id` BIGINT NOT NULL ,
+  `status` CHAR(1) NOT NULL ,
+  PRIMARY KEY (`record_id`) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
