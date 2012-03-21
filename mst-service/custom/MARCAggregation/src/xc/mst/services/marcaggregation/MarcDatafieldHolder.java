8e0d58965742c83c73b9982a0ea8c840d42b35ae
john.f.brand@gmail.com <john.f.brand@gmail.com@72d1a826-0e72-11de-b5bf-434079a3519f>
2012-03-21 20:06:00 +0000

    dynamic content changes, etc.
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/branches/marc_agg@3139 72d1a826-0e72-11de-b5bf-434079a3519f

--- mst-service/custom/MARCAggregation/src/xc/mst/services/marcaggregation/MarcDatafieldHolder.java
+++ mst-service/custom/MARCAggregation/src/xc/mst/services/marcaggregation/MarcDatafieldHolder.java
@@ -3,6 +3,12 @@ package xc.mst.services.marcaggregation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
+import xc.mst.services.marcaggregation.matcher.ISBNMatcher;
+import xc.mst.services.marcaggregation.matcher.ISSNMatcher;
+import xc.mst.services.marcaggregation.matcher.LccnMatcher;
+
 /**
  * A very specific container. This class encapsulates data fields that contain 1 or more subfields.
  * No error checking will be done for repeating data or enforcement of how many or few fields or few subfields
@@ -20,6 +26,8 @@ public class MarcDatafieldHolder {
     private String                   s_ind1;
     private String                   s_ind2;
     private List<MarcSubfieldHolder> s_subfields = new ArrayList<MarcSubfieldHolder>();
+
+    private static final Logger LOG = Logger.getLogger(MarcDatafieldHolder.class);
 
     public MarcDatafieldHolder(String datafield, List<MarcSubfieldHolder> subfields, String ind1, String ind2) {
         s_datafield = datafield;
@@ -42,53 +50,95 @@ public class MarcDatafieldHolder {
      *
      */
     public String toString() {
-        final String DATAFIELD_TAG = "marc:datafield";
-        final String SUBFIELD_TAG  = "marc:subfield";
-        final String IND1          = "ind1=";
-        final String IND2          = "ind2=";
-        final String TAG           = "tag=";
-        final String CODE          = "code=";
-        final String SP            = " ";
-        final String QU            = "\"";
-        final String LEFT          = "<";
-        final String LEFT_CLS      = "</";
-        final String RIGHT         = ">";
-        final String SEP           = System.getProperty("line.separator");
 
-        StringBuilder sb = new StringBuilder(LEFT);
-        sb.append(DATAFIELD_TAG).append(SP).append(IND1).append(QU).append(s_ind1).append(QU).append(SP);
-        sb.append(IND2).append(QU).append(s_ind2).append(QU).append(SP).append(TAG).append(QU).append(s_datafield).append(QU).append(RIGHT).append(SEP);
+        if (s_subfields.size() >0) {
+            final String DATAFIELD_TAG = "marc:datafield";
+            final String SUBFIELD_TAG  = "marc:subfield";
+            final String IND1          = "ind1=";
+            final String IND2          = "ind2=";
+            final String TAG           = "tag=";
+            final String CODE          = "code=";
+            final String SP            = " ";
+            final String QU            = "\"";
+            final String LEFT          = "<";
+            final String LEFT_CLS      = "</";
+            final String RIGHT         = ">";
+            final String SEP           = System.getProperty("line.separator");
 
-        for (MarcSubfieldHolder subfield: s_subfields) {
-            sb.append(LEFT).append(SUBFIELD_TAG).append(SP).append(CODE).append(QU).append(subfield.getSubfieldName()).append(QU).append(RIGHT);
-            sb.append(subfield.getSubfieldContents());
-            sb.append(LEFT_CLS).append(SUBFIELD_TAG).append(RIGHT).append(SEP);
+            StringBuilder sb = new StringBuilder(LEFT);
+            sb.append(DATAFIELD_TAG).append(SP).append(IND1).append(QU).append(s_ind1).append(QU).append(SP);
+            sb.append(IND2).append(QU).append(s_ind2).append(QU).append(SP).append(TAG).append(QU).append(s_datafield).append(QU).append(RIGHT).append(SEP);
+
+            for (MarcSubfieldHolder subfield: s_subfields) {
+                sb.append(LEFT).append(SUBFIELD_TAG).append(SP).append(CODE).append(QU).append(subfield.getSubfieldName()).append(QU).append(RIGHT);
+                sb.append(subfield.getSubfieldContents());
+                sb.append(LEFT_CLS).append(SUBFIELD_TAG).append(RIGHT).append(SEP);
+            }
+
+            sb.append(LEFT_CLS).append(DATAFIELD_TAG).append(RIGHT).append(SEP);
+
+            return sb.toString();
         }
-
-        sb.append(LEFT_CLS).append(DATAFIELD_TAG).append(RIGHT).append(SEP);
-
-        return sb.toString();
+        return "";
     }
 
-    // assumption that holds for now , and we rely on it:  there is only 1 subfield within the datafield
+    // assumption that holds for now , and we rely on it:  there is only 1 subfield within the data field
     public boolean equals(Object that) {
         if (that == null) return false;
         if ( this == that ) return true;
         if ( !(that instanceof MarcDatafieldHolder) ) return false;
 
         MarcDatafieldHolder h = (MarcDatafieldHolder) that;
+        return (marcHolderSubfieldsEqual(h));
+    }
+
+    // assumption that holds for now , and we rely on it:  there is only 1 subfield within the data field
+    protected boolean marcHolderSubfieldsEqual(MarcDatafieldHolder h) {
         if (this.getSubfields().size() >0  && h.getSubfields().size()>0) {
-            if (this.getSubfields().get(0).getSubfieldContents().equals(h.getSubfields().get(0).getSubfieldContents())) {
+            if (differentiatedMarcHolderSubfieldsEqual(h)) {
                 return true;
             }
         }
-        else if (this.getSubfields().size() == h.getSubfields().size()) {
+        else if (this.getSubfields().size() <1 && h.getSubfields().size() <1) {
             return true;
         }
         return false;
     }
 
+    //   it would be grand to do this in an object-oriented way, i.e. subclass this class overriding this method,
+    //   but for now use the lowly if statement.
+    protected boolean differentiatedMarcHolderSubfieldsEqual(MarcDatafieldHolder h) {
+
+        if (h.getDatafield().equals("035")) {
+            return  this.getSubfields().get(0).getSubfieldContents().equals(h.getSubfields().get(0).getSubfieldContents());
+        }
+        else if (h.getDatafield().equals("024")) {
+            return  this.getSubfields().get(0).getSubfieldContents().equals(h.getSubfields().get(0).getSubfieldContents());
+        }
+        else if (h.getDatafield().equals("010")) {
+            return  LccnMatcher.getUniqueId(this.getSubfields().get(0).getSubfieldContents()) ==
+                    LccnMatcher.getUniqueId(   h.getSubfields().get(0).getSubfieldContents());
+        }
+        else if (h.getDatafield().equals("020")) {
+
+            //LOG.info("*** 020:this->"+ISBNMatcher.getIsbn(this.getSubfields().get(0).getSubfieldContents())+"<-"   +
+            //        " that->"+ISBNMatcher.getIsbn(h.getSubfields().get(0).getSubfieldContents())+"<-");
+
+            return  ISBNMatcher.getIsbn(this.getSubfields().get(0).getSubfieldContents()).equals(
+                    ISBNMatcher.getIsbn(   h.getSubfields().get(0).getSubfieldContents()));
+        }
+        else if (h.getDatafield().equals("022")) {
+            return  ISSNMatcher.getAllButDash(this.getSubfields().get(0).getSubfieldContents()).equals(
+                    ISSNMatcher.getAllButDash(   h.getSubfields().get(0).getSubfieldContents()));
+        }
+        return false;
+    }
+
     public int hashCode() {
-        return (s_ind1+s_ind2+this.getSubfields().get(0).getSubfieldContents()+s_datafield).hashCode();
+        String s = "";
+        if (this.getSubfields().size()>0) {
+            s= this.getSubfields().get(0).getSubfieldContents();
+        }
+        return (s_ind1 + s_ind2+ s + s_datafield).hashCode();
     }
 }
