563cddbd455b01c41fbbdb3a99d29d275685fc6e
john.f.brand@gmail.com <john.f.brand@gmail.com@72d1a826-0e72-11de-b5bf-434079a3519f>
2012-05-21 18:22:54 +0000

    add test and files for MST-493
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/branches/marc_agg@3261 72d1a826-0e72-11de-b5bf-434079a3519f

---
+++ mst-service/custom/MARCAggregation/test/java/xc/mst/services/marcaggregation/test/MST_493_Test.java
@@ -0,0 +1,110 @@
+package xc.mst.services.marcaggregation.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This test is a bigger update/delete test.  Starts with records from the dedup directory, for this test called T0.xml.
+ * Then updates and deletes are applied in file T1.xml.
+ *
+ * Right now, I have failure commented out - I need to analyze num matchpoints expected, and what matches are expected,
+ * then will update the setupMatcherExpections method.
+ *
+ * @author John Brand
+ *
+ */
+public class MST_493_Test extends MatchRulesTest {
+
+    private static final Logger LOG = Logger.getLogger(DedupTest.class);
+
+    protected void setupMatcherExpectations() {
+        //load expected number of records for each matcher.
+        //TODO figure out how many of each till end of TODO
+        expectedMatchRecordIds.put("x028abMatcher", 0);
+        expectedMatchRecords.put  ("x028abMatcher", 0);
+
+        expectedMatchRecordIds.put("x245ahMatcher", 0);
+        expectedMatchRecords.put  ("x245ahMatcher", 0);
+
+        expectedMatchRecordIds.put("x240aMatcher", 0);
+        expectedMatchRecords.put  ("x240aMatcher", 0);
+
+        expectedMatchRecordIds.put("x260abcMatcher", 0);
+        expectedMatchRecords.put  ("x260abcMatcher", 0);
+
+        expectedMatchRecordIds.put("x130aMatcher", 0);
+        expectedMatchRecords.put  ("x130aMatcher", 0);
+        //TODO end of above TODO
+
+        expectedMatchRecordIds.put("ISSNMatcher", 14);  //confirmed
+        expectedMatchRecords.put  ("ISSNMatcher", 2);   //confirmed
+
+        expectedMatchRecordIds.put("ISBNMatcher", 78);  //TODO confirm both of these
+        expectedMatchRecords.put  ("ISBNMatcher", 23);
+
+        expectedMatchRecordIds.put("x024aMatcher", 3); //confirmed
+        expectedMatchRecords.put  ("x024aMatcher", 3); //confirmed
+
+        expectedMatchRecordIds.put("LccnMatcher", 49); //TODO confirm both of these
+        expectedMatchRecords.put  ("LccnMatcher", 8);
+
+        expectedMatchRecordIds.put("SystemControlNumberMatcher", 92); //TODO confirm both of these
+        expectedMatchRecords.put  ("SystemControlNumberMatcher", 27);
+
+
+        /*
+ rule 2 matchsets?:
+- *** Matchset: {62, 160, 201, }
+- *** Matchset: {10, 29, 52, 66, 132, 171, 205, 255, }
+- *** Matchset: {33, 71, 113, 137, 175, 207, 262, }
+
+ 035 matchsets?:
+- *** Matchset: {1, 25, 48, 94, 107, 167, 197, 234, 251, 316, }
+- *** Matchset: {3, 27, 50, 64, 96, 109, 162, 169, 203, 236, 253, 318, }
+- *** Matchset: {31, 68, 111, 134, 173, 238, 257, 320, }
+- *** Matchset: {12, 73, 98, 115, 177, 209, 264, 324, }
+- *** Matchset: {5, 14, 35, 54, 75, 100, 117, 179, 240, 267, 326, }
+- *** Matchset: {56, 58, 82, 121, 141, 164, 185, 211, 243, 275, 329, }
+- *** Matchset: {22, 60, 89, 104, 125, 154, 191, 222, 248, 308, 312, 333, }
+         */
+
+        expectedResults.add(getExpectedMatchSet(new long[]{5,14,35,54,75,100,117,179,240,267,326}));
+        expectedResults.add(getExpectedMatchSet(new long[]{22,60,89,104,125,154,191,222,248,308,312,333}));
+        expectedResults.add(getExpectedMatchSet(new long[]{56,58,82,121,141,164,185,211,243,275,329}));
+        expectedResults.add(getExpectedMatchSet(new long[]{12,73,98,115,177,209,264,324}));
+        expectedResults.add(getExpectedMatchSet(new long[]{31,68,111,134,173,238,257,320}));
+        expectedResults.add(getExpectedMatchSet(new long[]{3,27,50,64,96,109,162,169,203,236,253,318}));
+        expectedResults.add(getExpectedMatchSet(new long[]{1,25,48,94,107,167,197,234,251,316}));
+
+        expectedResults.add(getExpectedMatchSet(new long[]{62, 160, 201}));
+        expectedResults.add(getExpectedMatchSet(new long[]{10, 29, 52, 66, 132, 171, 205, 255}));
+        expectedResults.add(getExpectedMatchSet(new long[]{33, 71, 113, 137, 175, 207, 262}));
+    }
+
+    public List<String> getFolders() {
+        List<String> fileStrs = new ArrayList<String>();
+        fileStrs.add("MST-493");
+        return fileStrs;
+    }
+
+    protected String getTestName() {
+        return "MST_493_Test";
+    }
+
+
+    protected void reportFailure(String result) {
+        LOG.info(result);
+//        throw new RuntimeException(result);
+    }
+
+    protected void reportFailure(Exception e) {
+        LOG.info(e);
+//        throw new RuntimeException(e);
+    }
+
+
+}
+
+
