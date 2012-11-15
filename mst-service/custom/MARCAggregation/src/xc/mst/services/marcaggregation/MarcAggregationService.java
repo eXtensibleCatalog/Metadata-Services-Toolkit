ed013278c579db47979fe8ed45b9f392e499c877
cedelis@uillinois.edu <cedelis@uillinois.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2012-11-15 22:01:19 +0000

    I noticed that re-activating a deleted (aggregated) record was failing.
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/trunk@3472 72d1a826-0e72-11de-b5bf-434079a3519f

--- mst-service/custom/MARCAggregation/src/xc/mst/services/marcaggregation/MarcAggregationService.java
+++ mst-service/custom/MARCAggregation/src/xc/mst/services/marcaggregation/MarcAggregationService.java
@@ -993,7 +993,17 @@ public class MarcAggregationService extends GenericMetadataService {
             // unmerge type step, we will undo what has been done then redo from scratch, easiest to assure proper results.
             oldOutput = r.getSuccessors().get(0);
             results = cleanupOldMergedOutputInfo(union, results, true);
+            
+            /***
+             * I'm guessing we should really remerge the everything (union), not just the formerMatchSet.
+             * Because, how else would the newMatchedRecordIds ever get processed? Maybe an accidental
+             * typo from the past that's haunting us today (e.g., I attempted to re-activate a deleted
+             * record belonging to a match set, but it only resulted in a deleted output records and no
+             * new active output records).
             results.addAll(remerge(formerMatchSet));
+            ***/
+            results.addAll(remerge(union));
+            
         }
         else {   // same size merge set, must update.
             // this is the merge as you go along spot, and will be impacted if you change that paradigm.
@@ -1018,7 +1028,7 @@ public class MarcAggregationService extends GenericMetadataService {
             }
             // since same set, don't clean up any memory details, it all points to the right stuff still.
 
-            List<OutputRecord> list = null;
+//            List<OutputRecord> list = null;
             // may not have any matches!
             final boolean hasMatches = newMatchedRecordIds.size() > 0;
             String xml;
@@ -1052,13 +1062,17 @@ public class MarcAggregationService extends GenericMetadataService {
             // Add the updated record
             oldOutput.setType("b");
             results.add(oldOutput);
-
+/*
+ * * What's the purpose of the following??? Seems like just adding the same record, oldOutput, to the results a second time.
+ * I'm going to comment it out for now...
             list = new ArrayList<OutputRecord>();
             list.add(oldOutput);
 
             LOG.debug("** create unmerged output record: "+list.get(0).getId()+" status="+list.get(0).getStatus());
 
             results.addAll(list);
+*/   
+            
         }
         return results;
     }
