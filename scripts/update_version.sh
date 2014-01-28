87830062582417a1aaafa0c5ab717f32339d42e8
cedelis@uillinois.edu <cedelis@uillinois.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2014-01-28 18:05:20 +0000

    Make sed happier for MacOS X
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/trunk@3584 72d1a826-0e72-11de-b5bf-434079a3519f

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,20 +1,21 @@
 version=${1}
-sed -i "s/^mi.version=.*/mi.version=${version}/" ./MST-instances/build.properties 
-sed -i "s/^mc.version=.*/mc.version=${version}/" ./mst-common/build.properties
-sed -i "s/^mp.version=.*/mp.version=${version}/" ./mst-platform/build.properties
-sed -i "s/^ms.version=.*/ms.version=${version}/" ./mst-service/impl/build.properties
+sed -i ' ' "s/^mi.version=.*/mi.version=${version}/" ./MST-instances/build.properties 
+sed -i ' ' "s/^mc.version=.*/mc.version=${version}/" ./mst-common/build.properties
+sed -i ' ' "s/^mp.version=.*/mp.version=${version}/" ./mst-platform/build.properties
+sed -i ' ' "s/^ms.version=.*/ms.version=${version}/" ./mst-service/impl/build.properties
 
-sed -i "s/^mst-service-impl.version=.*/mst-service-impl.version=${version}/" ./mst-service/example/build.properties
-sed -i "s/^mst-instances.version=.*/mst-instances.version=${version}/" ./mst-service/example/build.properties
-sed -i "s/^service.version=.*/service.version=${version}/" ./mst-service/example/custom.properties
+sed -i ' ' "s/^mst-service-impl.version=.*/mst-service-impl.version=${version}/" ./mst-service/example/build.properties
+sed -i ' ' "s/^mst-instances.version=.*/mst-instances.version=${version}/" ./mst-service/example/build.properties
+sed -i ' ' "s/^service.version=.*/service.version=${version}/" ./mst-service/example/custom.properties
 
-ant.bat ms.copy-example
+#ant.bat ms.copy-example
+ant ms.copy-example
 
 for service in ./mst-service/custom/*
 do 
   echo $service
   cd $service
-  sed -i "s/^service.version=.*/service.version=${version}/" ./custom.properties
+  sed -i ' ' "s/^service.version=.*/service.version=${version}/" ./custom.properties
   cd ~-
 done
 
