8313961570de78682a194dcd4ab9cd54081cf6f7
cedelis@uillinois.edu <cedelis@uillinois.edu@72d1a826-0e72-11de-b5bf-434079a3519f>
2014-09-03 21:45:17 +0000

    Remove dropIndices. Doesn't seem appropriate (at least not at this juncture)
    git-svn-id: https://xcmetadataservicestoolkit.googlecode.com/svn/trunk@3611 72d1a826-0e72-11de-b5bf-434079a3519f

--- mst-common/src/java/xc/mst/repo/RepositoryDAO.java
+++ mst-common/src/java/xc/mst/repo/RepositoryDAO.java
@@ -579,10 +579,13 @@ public class RepositoryDAO extends BaseDAO {
                 LOG.debug("updateTime: " + new Date(updateTime));
 
                 LOG.debug("processedRecordsCount: " + processedRecordsCount);
+                /****
+                 * Why is this in here? I could maybe understand if you dropped indices *before* the updates (then later re-created them)...
                 LOG.debug("db.numInserts2dropIndexes: " + MSTConfiguration.getInstance().getPropertyAsInt("db.numInserts2dropIndexes", 0));
                 if (processedRecordsCount > MSTConfiguration.getInstance().getPropertyAsInt("db.numInserts2dropIndexes", 0)) {
                     dropIndices(name);
                 }
+                ****/
             } else {
                 try {
                     LOG.debug("recordsToAdd.size(): " + recordsToAdd.size());
