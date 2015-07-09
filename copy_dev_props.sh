09c30b73c18345316669f06d936d3e2ec2b98262
Chris Delis <cedelis@uillinois.edu>
2015-07-09 12:22:54 -0500

    Add new helper script for building source.

---
+++ copy_dev_props.sh
@@ -0,0 +1,10 @@
+cp ./mst-service/custom/DCToXCTransformation/dev.properties.example ./mst-service/custom/DCToXCTransformation/dev.properties
+
+cp ./mst-service/custom/MARCAggregation/dev.properties.example ./mst-service/custom/MARCAggregation/dev.properties
+
+cp ./mst-service/custom/MARCNormalization/dev.properties.example ./mst-service/custom/MARCNormalization/dev.properties
+
+cp ./mst-service/custom/MARCToXCTransformation/dev.properties.example ./mst-service/custom/MARCToXCTransformation/dev.properties
+
+cp ./mst-service/example/dev.properties.example ./mst-service/example/dev.properties
+
